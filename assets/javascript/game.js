2eca358fbe9de736568fde663c835aa8a5c8ebab
Dante <42275387+TJANGEL@users.noreply.github.com>
2018-10-09 10:50:41 -0400

    updates to css and js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,48 +5,53 @@ var startPoint = 0;
 
 var startGame = function () {
 
-    $(".btn").empty();
+$(".btn").empty();
 
-    matchThisNumber = Math.floor(Math.random() * 60) + 40;
+matchThisNumber = Math.floor(Math.random() * 69) + 30;
 
-    $(".matchThisNumber").html("Match This Number: " + matchThisNumber);
+$("#matchThisNumber").html('Match This Number: ' + matchThisNumber);
 
-    for (var i = 0; i < 4; i++) {
-        var randomValue = Math.floor(Math.random() * 9) + 1;
-        var crystal = $(".btn");
+for (var i = 0; i < 4; i++) {
 
-        $(".btn").append(crystal);
-    }
-
-    $("#startPoint").html(startPoint);
+    var randomValue = Math.floor(Math.random() * 9) + 1;
+    var crystal = $(".btn");
+        crystal.attr({
+            "class": 'btn',
+            "data-random": randomValue
+        })
+    $(".btn").append(crystal);
 }
 
+$("#startPoint").html('Your Number: ' + startPoint);
+}
 
 startGame();
 
 $(document).on("click", ".btn", function () {
-    var crystalValue = parseInt($(this).attr('NEEDS VALUE'));
+    var crystalValue = parseInt($(this).attr('data-random'));
 
     startPoint += crystalValue;
 
-    $("#startPoint").html(startPoint);
+    $("#startPoint").html('Your Number: ' + startPoint);
 
     if (startPoint > matchThisNumber) {
-        
+
         loss++
-        $("#loss").html("Losses: " + loss);
+        $("#loss").html("LOSSES: " + loss);
 
         startPoint = 0;
 
         startGame();
 
     } else if (startPoint === matchThisNumber) {
-        
+
         win++;
-        $("#win").html("Wins: " + win);
+        $("#win").html("WINS: " + win);
 
         startPoint = 0;
 
         startGame();
     }
+
+    console.log(crystalValue);
 });
